6bd6e8eb30e65d09bccfbdd637edaeafb684f4b2
Lissa Ferreira <lissadeveloper@gmail.com>
2021-01-27 01:24:29 -0400

    removing guake

--- arch/install-arch.sh
+++ arch/install-arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu
 
 #Installing programs using APT
-sudo pacman -S ffmpeg automake autoconf gparted xorg zsh curl audacity w3m supertux cmatrix frei0r-plugins git htop neofetch ranger snapd openssh tmux vim zathura sxiv traceroute sl guake base-devel gcc make postgresql ttf-fira-code supertux  ack xclip flameshot neovim pavucontrol composer bat rofi lxappearance feh compton polybar php obs-studio flameshot ruby nodejs dmenu go noto-fonts-emoji alacritty feh rofi sway swaylock compton ttf-droid ttf-dejavu ttf-ubuntu-font-family
+sudo pacman -S ffmpeg automake autoconf gparted xorg zsh curl audacity w3m supertux cmatrix frei0r-plugins git htop neofetch ranger snapd openssh tmux vim zathura sxiv traceroute sl base-devel gcc make postgresql ttf-fira-code supertux  ack xclip neovim pavucontrol composer bat rofi lxappearance feh compton polybar php obs-studio flameshot ruby nodejs dmenu go noto-fonts-emoji alacritty feh rofi sway swaylock compton ttf-droid ttf-dejavu ttf-ubuntu-font-family
 
 # Installing my Nvim config
 mkdir ~/.config/nvim
