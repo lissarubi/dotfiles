bcbe0e940c7dacbfaa79c82573580ebd2fe02bd9
Lissa Ferreira <lissadeveloper@gmail.com>
2021-02-10 23:21:54 -0400

    adding dmenu-edersonferreira-git to arch AUR

--- arch/install-arch.sh
+++ arch/install-arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu
 
 #Installing programs using APT
-sudo pacman -S ffmpeg automake autoconf gparted xorg zsh curl audacity w3m supertux cmatrix frei0r-plugins git htop neofetch ranger snapd openssh tmux vim zathura sxiv traceroute sl base-devel gcc make postgresql ttf-fira-code supertux  ack xclip neovim pavucontrol composer bat rofi lxappearance feh compton polybar php flameshot ruby nodejs dmenu go noto-fonts-emoji alacritty feh rofi sway swaylock compton ttf-droid ttf-dejavu ttf-ubuntu-font-family virtualbox linux59-virtualbox-host-modules virtualbox-guest-iso opendoas
+sudo pacman -S ffmpeg automake autoconf gparted xorg zsh curl audacity w3m supertux cmatrix frei0r-plugins git htop neofetch ranger snapd openssh tmux vim zathura sxiv traceroute sl base-devel gcc make postgresql ttf-fira-code supertux  ack xclip neovim pavucontrol composer bat lxappearance feh compton polybar php flameshot ruby nodejs go noto-fonts-emoji alacritty feh sway swaylock compton ttf-droid ttf-dejavu ttf-ubuntu-font-family virtualbox linux59-virtualbox-host-modules virtualbox-guest-iso opendoas
 
 # Installing my Nvim config
 mkdir ~/.config/nvim
@@ -36,7 +36,7 @@ makepkg -si
 
 # Installing AUR's
 
-paru flat-remix flat-remix-gtk obs-studio-git
+paru flat-remix flat-remix-gtk obs-studio-git bashtop dmenu-edersonferreira-git glow
 
 # Installing Virtual Box
 
