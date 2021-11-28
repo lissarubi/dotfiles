c5e52dc275439027cda3ef79a4b5e0987c03d132
Lissa Ferreira <lissadeveloper@gmail.com>
2021-11-28 13:43:31 -0400

    updating arch install script

--- arch/install-arch.sh
+++ arch/install-arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu
 
 #Installing programs using APT
-sudo pacman -S alacritty audacity autoconf automake base-devel bat cmatrix composer ctags curl doneack dunst feh ffmpeg firefox-nightly flameshot frei0r-plugins gcc git go gparted htop i3-gaps lsd lxappearance lxsession make neofetch nodejs noto-fonts-emoji opendoas openssh pavucontrol php polybar postgresql python-pip ranger ruby sl snapd supertux sxiv tmux traceroute ttf-dejavu ttf-droid ttf-fira-code ttf-ubuntu-font-family ueberzug vim virtualbox virtualbox-guest-iso w3m xclip xorg zathura-pdf-poppler zsh
+sudo pacman -S ack alacritty audacity autoconf automake base-devel bat cmatrix composer ctags curl dunst feh ffmpeg flameshot frei0r-plugins gcc git go gparted htop i3-gaps lsd lxappearance lxsession make neofetch nodejs npm noto-fonts-emoji opendoas openssh pavucontrol php postgresql python-pip ranger ruby sl supertux sxiv tmux traceroute ttf-dejavu ttf-droid ttf-fira-code ttf-ubuntu-font-family ueberzug vim virtualbox virtualbox-guest-iso w3m xclip xorg zathura-pdf-poppler zsh
 
 # Installing my Nvim config
 mkdir ~/.config/nvim
@@ -21,12 +21,6 @@ makepkg -si
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
-#Installing programs with Snap
-sudo snap install photogimp opera
-
-#Installing VSCode using Snap
-sudo snap install code --classic
-
 # Install Paru
 
 sudo pacman -S --needed base-devel
@@ -38,6 +32,7 @@ makepkg -si
 
 paru bibata-cursor-theme
 paru cef-minimal-3770-bin
+paru firefox-nightly
 paru flat-remix
 paru flat-remix-gtk
 paru fzf
@@ -50,10 +45,16 @@ paru openh264
 paru pacvis-git
 paru pamac-aur
 paru picom-jonaburg-git
+paru polybar
 paru python-ueberzug-git
 paru siji-git
+paru spotify
 paru ttf-devicons
 paru zoxide
+
+# Install pynvim
+
+python3 -m pip install --user --upgrade pynvim
 
 # Install my Dmenu build
 
@@ -69,6 +70,7 @@ sudo echo "vboxdrv" >> /etc/modules-load.d/virtualbox.conf
 sudo usermod -aG vboxusers $USER
 
 #Installing Kdenlive
+
 mkdir ~/software
 cd ~/software 
 wget https://files.kde.org/kdenlive/release/kdenlive-20.04.0-x86_64.appimage
@@ -77,16 +79,15 @@ mv kdenlive-20.04.0-x86_64.appimage kdenlive20.04.appimage
 cd ~
 
 #Installing programs using NPM
+
 sudo npm install prettier markpdf @prettier/plugin-php intelephense eslint eslint-plugin-vue -g
 
 #Installing Powerline10k theme
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 #pacman upgrade
 sudo pacman -Syu
-
-#Removing DEBS
-rm -rf ~/Downloads/*
 
 #Installing Oh My ZSH
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
